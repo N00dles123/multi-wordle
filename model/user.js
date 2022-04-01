ed80d7cc0d72da210be598603b69399dcf67af25
Jason <jasonlinfl@gmail.com>
2022-04-01 00:29:09 -0400

    registration done

--- model/user.js
+++ model/user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
     {
-    email: { type: String, required: true, unique: true, match:/.+\@.+\..+/},
-    username: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true},
+    uid: { type: String, required: true, unique: true},
     password: { type: String, required: true}
     },
     { collection: 'users'}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const User = require('./model/user');
+const bcrypt = require('bcryptjs');
 // so that token is not pushed to github
 require('dotenv').config();
 
@@ -21,7 +22,22 @@ app.use(bodyParser.json());
 app.post('/api/register', async (req, res) => {
     console.log(req.body);
 
-    // hash passwords to prevent stealing of data
+    const {email, uid, pwd} = req.body;
+    
+    // hash passwords to prevent stealing of data :)
+    const password = await bcrypt.hash(pwd, 10);
+    console.log(password);
+    try {
+        const res = await User.create({
+            email,
+            uid,
+            password
+        });
+        console.log("User created Successfully" , res);
+    } catch(error){
+        console.log(error);
+        return res.json({ status: 'error'});
+    }
 
     res.json({ status: 'ok' });
 });
